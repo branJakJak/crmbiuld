92573cea40a51ca139773dd7e1ec4cb569534a97
kevin daus <kevinflorenzdaus@gmail.com>
2017-07-14 20:18:09 +0800

    using google map

--- views/record/create.php
+++ views/record/create.php
@@ -30,57 +30,8 @@ $insulationType = [
     'EWI' => 'EWI'
 ];
 
-$this->registerJsFile('@web/js/crafty_postcode.class.js');
-$cccCode = <<<EOL
-    initAddressSearch();
-EOL;
-$this->registerJs($cccCode , \yii\web\View::POS_READY);
-
-$postalSearchCode = <<<EOL
-jQuery("#propertyrecord-postcode").blur(function(){
-    window.searchAddress()
-});
-EOL;
-$this->registerJs($postalSearchCode , \yii\web\View::POS_READY);
-
-
 
 ?>
-<script type="text/javascript">
-    var cp_obj;
-    function initAddressSearch(){
-        var _cp_token_fe = "90197-6843a-b808b-014ff";
-        var _cp_busy_img_url = '/img/crafty_postcode_busy.gif';
-        var _cp_err_msg1 = 'This postcode could not be found, please try again or enter your address manually';
-        var _cp_err_msg2 = 'This postcode is not valid, please try again or enter your address manually';
-        var _cp_err_msg3 = 'Unable to connect to address lookup server, please enter your address manually';
-        var _cp_err_msg4 = 'An unexpected error occurred, please enter your address manually';
-        cp_obj = CraftyPostcodeCreate();
-        // config
-        cp_obj.set("access_token", _cp_token_fe);
-        cp_obj.set("res_autoselect", "0");
-        cp_obj.set("result_elem_id", 'result-panel');
-        cp_obj.set("form", "");
-        cp_obj.set("elem_company"  , "");
-        cp_obj.set("elem_house_num", "");
-        cp_obj.set("elem_street1"  , 'propertyrecord-address1');
-        cp_obj.set("elem_street2"  , 'propertyrecord-address2');
-        cp_obj.set("elem_street3"  , 'propertyrecord-address3]');
-        cp_obj.set("elem_town"     , 'propertyrecord-town');
-        cp_obj.set("elem_county"   , ""); // optional
-        cp_obj.set("elem_postcode" , 'propertyrecord-postcode');
-        cp_obj.set("single_res_autoselect" , 1);
-        cp_obj.set("busy_img_url" , _cp_busy_img_url);
-        cp_obj.set("err_msg1", _cp_err_msg1);
-        cp_obj.set("err_msg2", _cp_err_msg2);
-        cp_obj.set("err_msg3", _cp_err_msg3);
-        cp_obj.set("err_msg4", _cp_err_msg4);
-    }
-    function searchAddress(){
-        cp_obj.doLookup();
-    }
-
-</script>
 
 <div class="row">
     <div class="col-lg-6">
@@ -96,12 +47,16 @@ $this->registerJs($postalSearchCode , \yii\web\View::POS_READY);
                 ->field($preCreatedRecord, 'insulation_type')
                 ->dropDownList($insulationType)
             ?>
-            <div id="result-panel"></div>
+            <hr>
+            <label>Address Search</label>
+            <?= Html::input('text','searchPostalCode','',['id'=>'searchPostalCode','class'=>'form-control'])?>
+            <hr>
             <?= $form->field($preCreatedRecord, 'postcode') ?>
             <?= $form->field($preCreatedRecord, 'address1') ?>
             <?= $form->field($preCreatedRecord, 'address2') ?>
             <?= $form->field($preCreatedRecord, 'address3') ?>
             <?= $form->field($preCreatedRecord, 'town') ?>
+            <?= $form->field($preCreatedRecord, 'country') ?>
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
         <?php ActiveForm::end(); ?>
         <?php
@@ -109,3 +64,52 @@ $this->registerJs($postalSearchCode , \yii\web\View::POS_READY);
         ?>
     </div>
 </div>
+
+
+<script type="text/javascript">
+    var placeSearch, autocomplete;
+    var componentForm = {
+        'propertyrecord-address1': 'long_name',
+        'propertyrecord-address2': 'long_name',
+        'propertyrecord-address3': 'long_name',
+        'propertyrecord-town': 'long_name',
+        'propertyrecord-country': 'long_name',
+        'propertyrecord-postcode':'short_name'
+    };
+    function initAutocomplete() {
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('searchPostalCode')),
+            {types: ['geocode']});
+        autocomplete.addListener('place_changed', fillInAddress);
+    }
+    function fillInAddress() {
+        var place = autocomplete.getPlace();
+        console.log(place);
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+        document.getElementById('propertyrecord-postcode').value = place.address_components[0].short_name;
+        document.getElementById('propertyrecord-address1').value = place.address_components[1].long_name;
+        document.getElementById('propertyrecord-address2').value = place.address_components[2].long_name;
+        document.getElementById('propertyrecord-address3').value = place.address_components[3].long_name;
+        document.getElementById('propertyrecord-town').value = place.address_components[4].long_name;
+        document.getElementById('propertyrecord-country').value = place.address_components[5].long_name;
+    }
+    function geolocate() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var geolocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                var circle = new google.maps.Circle({
+                    center: geolocation,
+                    radius: position.coords.accuracy
+                });
+                autocomplete.setBounds(circle.getBounds());
+            });
+        }
+    }
+</script>
+<script src="//maps.googleapis.com/maps/api/js?key=AIzaSyBspZlwYVvLnDac07_LpMxtpXgEtA-p-dU&libraries=places&callback=initAutocomplete" async defer></script>
