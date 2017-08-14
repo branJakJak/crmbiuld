80d29181c2976e29c6e62a29cc8a7ce36db075dd
kevin daus <kevinflorenzdaus@gmail.com>
2017-08-15 05:09:13 +0800

    asd

---
+++ rbac2/assignments.php
@@ -0,0 +1,66 @@
+<?php
+return [
+    1 => [
+        'admin',
+    ],
+    7 => [
+        'Senior Manager',
+    ],
+    9 => [
+        'Manager',
+    ],
+    10 => [
+        'Senior Manager',
+    ],
+    11 => [
+        'Senior Manager',
+    ],
+    12 => [
+        'Manager',
+    ],
+    13 => [
+        'Consultant',
+    ],
+    14 => [
+        'Admin',
+    ],
+    15 => [
+        'Senior Manager',
+    ],
+    16 => [
+        'Manager',
+    ],
+    17 => [
+        'Consultant',
+    ],
+    18 => [
+        'Admin',
+    ],
+    19 => [
+        'Manager',
+    ],
+    20 => [
+        'Consultant',
+    ],
+    2 => [
+        'Senior Manager',
+    ],
+    3 => [
+        'Manager',
+    ],
+    4 => [
+        'Manager',
+    ],
+    5 => [
+        'Consultant',
+    ],
+    6 => [
+        'Consultant',
+    ],
+    21 => [
+        'Agent',
+    ],
+    22 => [
+        'Agent',
+    ],
+];
---
+++ rbac2/items.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    'admin' => [
+        'type' => 1,
+    ],
+    'agent' => [
+        'type' => 1,
+    ],
+    'Senior Manager' => [
+        'type' => 1,
+    ],
+    'Manager' => [
+        'type' => 1,
+        'children' => [
+            'managerPermission',
+        ],
+    ],
+    'Consultant' => [
+        'type' => 1,
+        'children' => [
+            'editOwnRecordPermission',
+        ],
+    ],
+    'editOwnRecordPermission' => [
+        'type' => 2,
+        'description' => 'edit record owned by this',
+        'ruleName' => 'isConsultant',
+    ],
+    'managerPermission' => [
+        'type' => 2,
+        'description' => 'manager permission',
+        'ruleName' => 'isManager',
+    ],
+    'Admin' => [
+        'type' => 1,
+    ],
+    'Agent' => [
+        'type' => 1,
+    ],
+];
