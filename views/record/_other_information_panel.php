7ea276223109513afb23615e29763d4348e6305b
kevin daus <kevinflorenzdaus@gmail.com>
2017-09-09 03:11:08 +0800

    dsa

--- views/record/_other_information_panel.php
+++ views/record/_other_information_panel.php
@@ -5,14 +5,14 @@
 use derekisbusy\panel\PanelWidget;
 use yii\widgets\DetailView;
 
-
-$imageCollection =[];
-$allSupportingDocuments = $cavityModel->getSupportingDocuments()->all();
-foreach ($allSupportingDocuments as $currentSupportingDocument) {
-    $imageToPublish = Yii::getAlias("@supporting_document_path") . DIRECTORY_SEPARATOR . $currentSupportingDocument->document_name;
-    $published = $this->assetManager->publish($imageToPublish);
-    $imageCollection[] = $published[1];
-}
+//
+//$imageCollection =[];
+//$allSupportingDocuments = $cavityModel->getSupportingDocuments()->all();
+//foreach ($allSupportingDocuments as $currentSupportingDocument) {
+//    $imageToPublish = Yii::getAlias("@supporting_document_path") . DIRECTORY_SEPARATOR . $currentSupportingDocument->document_name;
+//    $published = $this->assetManager->publish($imageToPublish);
+//    $imageCollection[] = $published[1];
+//}
 
 
 ?>
@@ -77,64 +77,6 @@ foreach ($allSupportingDocuments as $currentSupportingDocument) {
         </div>
 
     </div>
-    <div class="col-lg-12">
-        <?php
-        echo PanelWidget::begin([
-            'title' => 'Uploaded Images',
-            'type' => 'default',
-            'widget' => false,
-        ])
-        ?>
-
-        <?= dosamigos\gallery\Gallery::widget(['items' => $imageCollection]);?>
 
 
-        <?php
-        //         GridView::widget([
-        //             'dataProvider' => $dataProvider,
-        //             'columns' => [
-        // //                ['class' => 'yii\grid\SerialColumn'],
-        // //                'id',
-        // //                'document_name',
-        //                 [
-        //                     'label'=>'Document',
-        //                     'value'=>function($model){
-        //                         $mes = 'Not Specified';
-        //                         if($model->type === \app\models\CavitySupportingDocument::FILE_TYPE_GUARANTOR_CERTIFICATE){
-        //                             $mes = 'Guarantee cert or communication confirming which company installed the cavity showing date when installed.';
-        //                         } else if($model->type === \app\models\CavitySupportingDocument::FILE_TYPE_PHOTO){
-        //                             $mes = 'Photo ID ie driving licence or passport';
-        //                         } else if($model->type === \app\models\CavitySupportingDocument::FILE_TYPE_PROOF_OF_ADDRESS){
-        //                             $mes = 'Proof of address';
-        //                         } else if ($model->type === 'internal_images' ){
-        //                             $mes = 'Proof of address';
-        //                         } else if ($model->type === 'external_images' ){
-        //                             $mes = 'External Images';
-        //                         } else if ($model->type === 'supporting_document_images' ){
-        //                             $mes = 'Supporting Document';
-        //                         }
-
-        //                         $htmlOptions = [
-        //                             'data-toggle'=>"tooltip",
-        //                             'title'=>$mes,
-        //                         ];
-        //                         return Html::a($model->document_name, \yii\helpers\Url::to(['cavity-supporting-document/download', 'id' => $model->id]) , $htmlOptions);
-        //                     },
-        //                     'format'=>'raw',
-        //                     'attribute'=>'document_name'
-        //                 ],
-
-        // //                'type',
-        // //                'document_name',
-        // //                'date_created',
-        // //                ['class' => 'yii\grid\ActionColumn'],
-        //             ],
-        //         ]);
-        ?>
-
-        <?php
-        PanelWidget::end()
-        ?>
-
-    </div>
 </div>
