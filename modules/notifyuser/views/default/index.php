798543a604bfc3c53a5cb8f2ae6b5e21b2be7cb1
kevin daus <kevinflorenzdaus@gmail.com>
2017-09-14 17:17:09 +0800

    changed label

--- modules/notifyuser/views/default/index.php
+++ modules/notifyuser/views/default/index.php
@@ -11,8 +11,8 @@ use derekisbusy\panel\PanelWidget;
     ])
     ?>
     <?php $form = \yii\widgets\ActiveForm::begin(['id'=>'updateStatusForm']) ?>
-    <?= $form->field($model, 'new_lead_notify')->textarea(['rows' => 6])->label('Email address to notify when new lead went to Pending Admin Approval ')->hint('One email address per line') ?>
-    <?= $form->field($model, 'change_lead_notify')->textarea(['rows' => 6])->label('Email address to notify when new lead went to Pending Admin Approval ')->hint('One email address per line') ?>
+    <?= $form->field($model, 'new_lead_notify')->textarea(['rows' => 6])->label('Email address to notify when new lead went is received')->hint('One email address per line') ?>
+    <?= $form->field($model, 'change_lead_notify')->textarea(['rows' => 6])->label('Email address to notify when a lead went to Pending Admin Approval ')->hint('One email address per line') ?>
     <?= \yii\helpers\Html::submitButton("Submit",['class'=>'btn btn-primary'])?>
 
     <?php \yii\widgets\ActiveForm::end() ?>
