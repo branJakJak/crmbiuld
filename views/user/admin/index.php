1e8241263e164fc93947baa0b332d3fa8b700d27
kevin daus <kevinflorenzdaus@gmail.com>
2017-09-14 22:03:41 +0800

    remove block status

--- views/user/admin/index.php
+++ views/user/admin/index.php
@@ -120,25 +120,25 @@ $this->params['breadcrumbs'][] = $this->title;
             'format' => 'raw',
             'visible' => Yii::$app->getModule('user')->enableConfirmation,
         ],
-        [
-            'header' => Yii::t('user', 'Block status'),
-            'value' => function ($model) {
-                if ($model->isBlocked) {
-                    return Html::a(Yii::t('user', 'Unblock'), ['block', 'id' => $model->id], [
-                        'class' => 'btn btn-xs btn-success btn-block',
-                        'data-method' => 'post',
-                        'data-confirm' => Yii::t('user', 'Are you sure you want to unblock this user?'),
-                    ]);
-                } else {
-                    return Html::a(Yii::t('user', 'Block'), ['block', 'id' => $model->id], [
-                        'class' => 'btn btn-xs btn-danger btn-block',
-                        'data-method' => 'post',
-                        'data-confirm' => Yii::t('user', 'Are you sure you want to block this user?'),
-                    ]);
-                }
-            },
-            'format' => 'raw',
-        ],
+        // [
+        //     'header' => Yii::t('user', 'Block status'),
+        //     'value' => function ($model) {
+        //         if ($model->isBlocked) {
+        //             return Html::a(Yii::t('user', 'Unblock'), ['block', 'id' => $model->id], [
+        //                 'class' => 'btn btn-xs btn-success btn-block',
+        //                 'data-method' => 'post',
+        //                 'data-confirm' => Yii::t('user', 'Are you sure you want to unblock this user?'),
+        //             ]);
+        //         } else {
+        //             return Html::a(Yii::t('user', 'Block'), ['block', 'id' => $model->id], [
+        //                 'class' => 'btn btn-xs btn-danger btn-block',
+        //                 'data-method' => 'post',
+        //                 'data-confirm' => Yii::t('user', 'Are you sure you want to block this user?'),
+        //             ]);
+        //         }
+        //     },
+        //     'format' => 'raw',
+        // ],
         [
             'class' => 'yii\grid\ActionColumn',
             'template' => '{switch} {resend_password} {update} {delete}',
