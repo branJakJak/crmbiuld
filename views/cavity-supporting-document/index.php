605c0ac808ada4a622a9695e64962a4eef833216
DESKTOP-SLBD6LO\User <bran_jakjak@gmail.com>
2017-10-18 02:54:16 +0800

    ADDED - document_description to the view record

--- views/cavity-supporting-document/index.php
+++ views/cavity-supporting-document/index.php
@@ -25,6 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'cavity_form_id',
             'type',
             'document_name',
+            'document_description',
             'date_created',
             // 'date_updated',
 
